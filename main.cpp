590606bd215581a1679abe486441cb53d0d89007
IvanOG7777 <elderivanargueta04@gmail.com>
2025-08-16 23:21:39 -0700

    Truck class

--- main.cpp
+++ main.cpp
@@ -24,22 +24,44 @@ enum class Orientation {
 class Car {
 public:
     int carLength;
-    std::vector<int> carVector;
+    std::vector<char> carVector;
     Car() : carLength(2) {carVector = initalizeCarVector();}
 
     void printCar() {
-        for (int i : carVector) {
+        for (char i : carVector) {
             std::cout << i;
         }
     }
 
 private:
-     std:: vector<int> initalizeCarVector() {
-        std:: vector<int> carVector(carLength);
-        for (int & i : carVector) {
+     std:: vector<char> initalizeCarVector() {
+        std:: vector<char> carVector(carLength);
+        for (char &i : carVector) {
             i = 'c';
         }
         return carVector;
+    }
+};
+
+class Truck {
+public:
+    int truckLength;
+    std::vector<char> truckVector;
+    Truck() : truckLength(3) {truckVector = initalizeTruckVector();}
+
+    void printTruck() {
+        for (char i : truckVector) {
+            std::cout << i;
+        }
+    }
+
+private:
+    std:: vector<char> initalizeTruckVector() {
+        std:: vector<char> truckVector(truckLength);
+        for (char &i : truckVector) {
+            i = 't';
+        }
+        return truckVector;
     }
 };
 
@@ -47,7 +69,7 @@ class Board {
     public:
     int height;
     int width;
-    std::vector<std::vector<int>> grid;
+    std::vector<std::vector<char>> grid;
     Board(int passedHeight, int passedWidth) : height(passedHeight), width(passedWidth) {grid = initalizeBoard();}
 
     void printBoard() {
@@ -59,17 +81,43 @@ class Board {
         }
     }
 
-    void placePiece(Car &car, std:: vector<std:: vector<int>> &board) {
-
+    void placeCarPiece(Car &car, std:: vector<std:: vector<char>> &board, int xCoord, int yCoord) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[yCoord][xCoord] == '0') {
+                    board[yCoord][xCoord] = car.carVector[0];
+                }
+                if (board[yCoord][xCoord + 1] == '0')
+                    board[yCoord][xCoord + 1] = car.carVector[1];
+            }
+        }
     }
 
+    void placeTruckPiece(Truck &truck, std:: vector<std:: vector<char>> &board, int xCoord, int yCoord) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[yCoord][xCoord] == '0') {
+                    board[yCoord][xCoord] = truck.truckVector[0];
+                }
+                if (board[yCoord][xCoord + 1] == '0') {
+                    board[yCoord][xCoord + 1] = truck.truckVector[1];
+                }
+                if (board[yCoord][xCoord + 2] == '0') {
+                    board[yCoord][xCoord + 2] = truck.truckVector[2];
+                }
+            }
+        }
+    }
+
+
+
 private:
-    std:: vector<std:: vector<int>> initalizeBoard() {
-        std::vector<std:: vector<int>> board(height, std::vector<int>(width, 0));
+    std:: vector<std:: vector<char>> initalizeBoard() {
+        std::vector<std:: vector<char>> board(height, std::vector<char>(width, '0'));
 
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board[0].size(); j++) {
-                board[i][j] = 0;
+                board[i][j] = '0';
             }
         }
         return board;
@@ -81,17 +129,24 @@ private:
 
 int main() {
 
-    std:: vector<int> Truck = {1,2,3};
-
     Car car;
-    std:: cout << "Car length "<< car.carLength << std::endl;
-    std:: cout << "Car Vector ";
-    car.printCar();
+    Truck truck;
 
+    std:: cout << "Truck Length: " << truck.truckLength << std::endl;
+    std:: cout << "Truck vector: ";
+    truck.printTruck();
     std:: cout << std:: endl;
 
-    Board boardclass(BOARD_HEIGHT, BOARD_WIDTH);
+    std:: cout << "Initialized Board game" << std:: endl;
+    Board board(BOARD_HEIGHT, BOARD_WIDTH);
 
-    boardclass.printBoard();
+    board.printBoard();
+
+    std:: cout <<std:: endl;
+
+    board.placeCarPiece(car, board.grid, 0, 0);
+    board.placeTruckPiece(truck, board.grid, 2, 2);
+
+    board.printBoard();
 
 }
