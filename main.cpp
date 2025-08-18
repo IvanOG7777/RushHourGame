e30bd84b1d18856b4f90ad9a50ebf26c766c5e92
IvanOG7777 <elderivanargueta04@gmail.com>
2025-08-18 12:09:13 -0700

    Refectoring of functions
    Condensed placement functions into one big one for placing pieces that now checks both vertical and horizontal placements, will add a collision detector next

--- main.cpp
+++ main.cpp
@@ -25,18 +25,18 @@ class Car {
 public:
     int carLength;
     std::vector<char> carVector;
-    Car() : carLength(2) {carVector = initalizeCarVector();}
+    Car() : carLength(2) { carVector = initalizeCarVector(); }
 
     void printCar() {
-        for (char i : carVector) {
-            std:: cout << i;
+        for (char i: carVector) {
+            std::cout << i;
         }
     }
 
 private:
-     std:: vector<char> initalizeCarVector() {
-        std:: vector<char> carVector(carLength);
-        for (char &i : carVector) {
+    std::vector<char> initalizeCarVector() {
+        std::vector<char> carVector(carLength);
+        for (char &i: carVector) {
             i = 'c';
         }
         return carVector;
@@ -47,18 +47,18 @@ class Truck {
 public:
     int truckLength;
     std::vector<char> truckVector;
-    Truck() : truckLength(3) {truckVector = initalizeTruckVector();}
+    Truck() : truckLength(3) { truckVector = initalizeTruckVector(); }
 
     void printTruck() {
-        for (char i : truckVector) {
-            std:: cout << i;
+        for (char i: truckVector) {
+            std::cout << i;
         }
     }
 
 private:
-    std:: vector<char> initalizeTruckVector() {
-        std:: vector<char> truckVector(truckLength);
-        for (char &i : truckVector) {
+    std::vector<char> initalizeTruckVector() {
+        std::vector<char> truckVector(truckLength);
+        for (char &i: truckVector) {
             i = 't';
         }
         return truckVector;
@@ -66,30 +66,30 @@ private:
 };
 
 class Board {
-    public:
+public:
     int height;
     int width;
-    std::vector<std::vector<char>> grid;
-    Board(int passedHeight, int passedWidth) : height(passedHeight), width(passedWidth) {grid = initalizeBoard();}
+    std::vector<std::vector<char> > grid;
+    Board(int passedHeight, int passedWidth) : height(passedHeight), width(passedWidth) { grid = initalizeBoard(); }
 
     void newPrintBoard() {
-        for (auto row : grid) {
-            for (auto element : row) {
+        for (auto row: grid) {
+            for (auto element: row) {
                 if (element == 'c') {
-                    std:: cout << "\033[31m" << element << "\033[0m" << " ";
+                    std::cout << "\033[31m" << element << "\033[0m" << " ";
                 } else if (element == 't') {
-                    std:: cout << "\033[32m" << element << "\033[0m" << " ";
+                    std::cout << "\033[32m" << element << "\033[0m" << " ";
                 } else {
-                    std:: cout << element << " ";
+                    std::cout << element << " ";
                 }
             }
-            std:: cout << std::endl;
+            std::cout << std::endl;
         }
     }
 
-    bool isInbounds (std:: vector<char> &pieceVector, std:: vector< std:: vector<char>> &board, int xCoord, int yCoord, bool isVertical) {
+    bool isInbounds(std::vector<char> &pieceVector, std::vector<std::vector<char> > &board, int xCoord, int yCoord,
+                    bool isVertical) {
         bool inBounds = false;
-        int negativeDirection = -1;
 
         if (board.empty() || board[0].empty()) {
             return inBounds;
@@ -98,36 +98,41 @@ class Board {
         int row = static_cast<int>(board.size());
         int col = static_cast<int>(board[0].size());
         int length = static_cast<int>(pieceVector.size());
-        if ((isVertical == false)) { // checks the horizontal bounds of the piece that is being placed only checks piece being printed left to right
-            for (int i = 0; i < length; i++) {
-                int newX = x + i * negativeDirection;
-                if (newX < 0 || newX >= col) {
-                    inBounds = false;
-                    return inBounds;
-                }
-            }
 
-            if (
+        if ((isVertical == false)) {
+            // checks the horizontal bounds of the piece that is being placed
+
+            if ( // checks piece doing from left to right
                 (yCoord >= 0 && yCoord < row) &&
                 (xCoord >= 0) &&
                 (xCoord + length - 1 < col)
-                ) {
+            ) {
                 inBounds = true;
             }
-        } else if (isVertical == true) { // checks the vertical bounds of the piece that is being placed only checks piece being printed up to down
-            for (int i = 0; i < length; i++) {
-                int newY = y + i * negativeDirection;
-                if (newY < 0 || newY >= row) {
-                    inBounds = false;
-                    return inBounds;
-                }
-            }
 
-            if (
+            if ( // checks piece doing from right to left
+                (yCoord >= 0 && yCoord < row) &&
+                (xCoord >= 0) &&
+                (xCoord - (length - 1) >= 0)
+            ) {
+                inBounds = true;
+            }
+        } else if (isVertical == true) {
+            // checks the vertical bounds of the piece that is being placed
+
+            if ( // checks vertical going from up to down
                 (xCoord >= 0 && xCoord < col) &&
                 (yCoord >= 0) &&
                 (yCoord + length - 1 < row)
-                ) {
+            ) {
+                inBounds = true;
+            }
+
+            if ( // checks vertical going from down to up
+                (xCoord >= 0 && xCoord < col) &&
+                (yCoord >= 0) &&
+                (yCoord - (length - 1) >= 0)
+            ) {
                 inBounds = true;
             }
         }
@@ -136,110 +141,144 @@ class Board {
     }
 
     //currently places the car piece horizontally
-    void placeCarPiece(Car &car, std:: vector<std:: vector<char>> &board, int xCoord, int yCoord) {
-
-        if (isInbounds(car.carVector, board, xCoord, yCoord, false) == false) {
-            std:: cout << "Cant place piece" << std:: endl;
+    void placeCarPiece(Car &car, std::vector<std::vector<char> > &board, int xCoord, int yCoord, bool isVertical) {
+        if (isInbounds(car.carVector, board, xCoord, yCoord, isVertical) == false) {
+            std::cout << "Cant place piece" << std::endl;
             return;
         }
 
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-                if (board[yCoord][xCoord] == '0') {
-                    board[yCoord][xCoord] = car.carVector[0];
+        int row = static_cast<int>(board.size());
+        int col = static_cast<int>(board[0].size());
+        int length = static_cast<int>(car.carVector.size());
+
+        if (isVertical == false) {
+            if ((yCoord >= 0 && yCoord < row) && (xCoord >= 0) && (xCoord + length - 1 < col)) {
+                for (int i = 0; i < board.size(); i++) {
+                    for (int j = 0; j < board[0].size(); j++) {
+                        if (board[yCoord][xCoord] == '0') {
+                            board[yCoord][xCoord] = car.carVector[0];
+                        }
+                        if (board[yCoord][xCoord + 1] == '0')
+                            board[yCoord][xCoord + 1] = car.carVector[1];
+                    }
                 }
-                if (board[yCoord][xCoord + 1] == '0')
-                    board[yCoord][xCoord + 1] = car.carVector[1];
+            } else if ((yCoord >= 0 && yCoord < row) && (xCoord >= 0) && (xCoord - (length - 1) >= 0)) {
+                for (int i = 0; i < board.size(); i++) {
+                    for (int j = 0; j < board[0].size(); j++) {
+                        if (board[yCoord][xCoord] == '0') {
+                            board[yCoord][xCoord] = car.carVector[0];
+                        }
+                        if (board[yCoord][xCoord - 1] == '0')
+                            board[yCoord][xCoord - 1] = car.carVector[1];
+                    }
+                }
             }
         }
-    }
 
-    void placeCarPieceVertical(Car &car, std:: vector<std:: vector<char>> &board, int xCoord, int yCoord) {
-
-        if (isInbounds(car.carVector, board, xCoord, yCoord, true) == false) {
-            std:: cout << "Cant place piece" << std:: endl;
-            return;
-        }
-
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-                if (board[yCoord][xCoord] == '0') {
-                    board[yCoord][xCoord] = car.carVector[0];
+        if (isVertical == true) {
+            if ((xCoord >= 0 && xCoord < col) && (yCoord >= 0) && (yCoord + length - 1 < row)) {
+                for (int i = 0; i < board.size(); i++) {
+                    for (int j = 0; j < board[0].size(); j++) {
+                        if (board[yCoord][xCoord] == '0') {
+                            board[yCoord][xCoord] = car.carVector[0];
+                        }
+                        if (board[yCoord + 1][xCoord] == '0')
+                            board[yCoord + 1][xCoord] = car.carVector[1];
+                    }
                 }
-                if (board[yCoord + 1][xCoord] == '0')
-                    board[yCoord + 1][xCoord] = car.carVector[1];
+            } else if ((xCoord >= 0 && xCoord < col) && (yCoord >= 0) && (yCoord - (length - 1) >= 0)) {
+                for (int i = 0; i < board.size(); i++) {
+                    for (int j = 0; j < board[0].size(); j++) {
+                        if (board[yCoord][xCoord] == '0') {
+                            board[yCoord][xCoord] = car.carVector[0];
+                        }
+                        if (board[yCoord - 1][xCoord] == '0')
+                            board[yCoord - 1][xCoord] = car.carVector[1];
+                    }
+                }
             }
         }
     }
 
     //currently places the truck piece horizontally
-    void placeTruckPiece(Truck &truck, std:: vector<std:: vector<char>> &board, int xCoord, int yCoord) {
-
-        if (isInbounds(truck.truckVector, board, xCoord, yCoord, false) == false) {
-            std:: cout << "Cant place piece" << std:: endl;
+    void placeTruckPiece(Truck &truck, std::vector<std::vector<char> > &board, int xCoord, int yCoord, bool isVertical) {
+        if (isInbounds(truck.truckVector, board, xCoord, yCoord, isVertical) == false) {
+            std::cout << "Cant place piece" << std::endl;
             return;
         }
 
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-                if (board[yCoord][xCoord] == '0') {
-                    board[yCoord][xCoord] = truck.truckVector[0];
+        int row = static_cast<int>(board.size());
+        int col = static_cast<int>(board[0].size());
+        int length = static_cast<int>(truck.truckVector.size());
+
+        if (isVertical == false) {
+            if ((yCoord >= 0 && yCoord < row) && (xCoord >= 0) && (xCoord + length - 1 < col)) {
+                for (int i = 0; i < board.size(); i++) {
+                    for (int j = 0; j < board[0].size(); j++) {
+                        if (board[yCoord][xCoord] == '0') {
+                            board[yCoord][xCoord] = truck.truckVector[0];
+                        }
+                        if (board[yCoord][xCoord + 1] == '0') {
+                            board[yCoord][xCoord + 1] = truck.truckVector[1];
+                        }
+                        if (board[yCoord][xCoord + 2] == '0') {
+                            board[yCoord][xCoord + 2] = truck.truckVector[2];
+                        }
+                    }
                 }
-                if (board[yCoord][xCoord + 1] == '0') {
-                    board[yCoord][xCoord + 1] = truck.truckVector[1];
+            } else if ((yCoord >= 0 && yCoord < row) && (xCoord >= 0) && (xCoord - (length - 1) >= 0)){
+                for (int i = 0; i < board.size(); i++) {
+                    for (int j = 0; j < board[0].size(); j++) {
+                        if (board[yCoord][xCoord] == '0') {
+                            board[yCoord][xCoord] = truck.truckVector[0];
+                        }
+                        if (board[yCoord][xCoord - 1] == '0') {
+                            board[yCoord][xCoord - 1] = truck.truckVector[1];
+                        }
+                        if (board[yCoord][xCoord - 2] == '0') {
+                            board[yCoord][xCoord - 2] = truck.truckVector[2];
+                        }
+                    }
                 }
-                if (board[yCoord][xCoord + 2] == '0') {
-                    board[yCoord][xCoord + 2] = truck.truckVector[2];
+            }
+        }
+
+        if (isVertical == true) {
+            if ((xCoord >= 0 && xCoord < col) && (yCoord >= 0) && (yCoord + length - 1 < row)) {
+                for (int i = 0; i < board.size(); i++) {
+                    for (int j = 0; j < board[0].size(); j++) {
+                        if (board[yCoord][xCoord] == '0') {
+                            board[yCoord][xCoord] = truck.truckVector[0];
+                        }
+                        if (board[yCoord + 1][xCoord] == '0') {
+                            board[yCoord + 1][xCoord] = truck.truckVector[1];
+                        }
+                        if (board[yCoord + 2][xCoord] == '0') {
+                            board[yCoord + 2][xCoord] = truck.truckVector[2];
+                        }
+                    }
+                }
+            } else if ((xCoord >= 0 && xCoord < col) && (yCoord >= 0) && (yCoord - (length - 1) >= 0)){
+                for (int i = 0; i < board.size(); i++) {
+                    for (int j = 0; j < board[0].size(); j++) {
+                        if (board[yCoord][xCoord] == '0') {
+                            board[yCoord][xCoord] = truck.truckVector[0];
+                        }
+                        if (board[yCoord - 1][xCoord] == '0') {
+                            board[yCoord - 1][xCoord] = truck.truckVector[1];
+                        }
+                        if (board[yCoord - 2][xCoord] == '0') {
+                            board[yCoord - 2][xCoord] = truck.truckVector[2];
+                        }
+                    }
                 }
             }
         }
     }
-
-    void placeTruckPieceVertical(Truck &truck, std:: vector<std:: vector<char>> &board, int xCoord, int yCoord) {
-
-        if (isInbounds(truck.truckVector, board, xCoord, yCoord, true) == false) {
-            std:: cout << "Cant place piece" << std:: endl;
-            return;
-        }
-
-        if (canPlacePieceVertical(truck.truckVector, board, xCoord, yCoord, true, 1)) {
-            for (int row = 0; row < board.size(); row++) {
-                for (int col = 0; col < board[0].size(); col++) {
-                    if (board[yCoord][xCoord] == '0') {
-                        board[yCoord][xCoord] = truck.truckVector[0];
-                    }
-                    if (board[yCoord + 1][xCoord] == '0') {
-                        board[yCoord + 1][xCoord] = truck.truckVector[1];
-                    }
-                    if (board[yCoord + 2][xCoord] == '0') {
-                        board[yCoord + 2][xCoord] = truck.truckVector[2];
-                    }
-                }
-            }
-        }
-
-        if (canPlacePieceVertical(truck.truckVector, board, xCoord, yCoord, true, -1)) {
-            for (int row = 0; row < board.size(); row++) {
-                for (int col = 0; col < board[0].size(); col++) {
-                    if (board[yCoord][xCoord] == '0') {
-                        board[yCoord][xCoord] = truck.truckVector[0];
-                    }
-                    if (board[yCoord - 1][xCoord] == '0') {
-                        board[yCoord - 1][xCoord] = truck.truckVector[1];
-                    }
-                    if (board[yCoord - 2][xCoord] == '0') {
-                        board[yCoord - 2][xCoord] = truck.truckVector[2];
-                    }
-                }
-            }
-        }
-    }
-
-
 
 private:
-    std:: vector<std:: vector<char>> initalizeBoard() {
-        std::vector<std:: vector<char>> board(height, std::vector<char>(width, '0'));
+    std::vector<std::vector<char> > initalizeBoard() {
+        std::vector<std::vector<char> > board(height, std::vector<char>(width, '0'));
 
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board[0].size(); j++) {
@@ -251,29 +290,25 @@ private:
 };
 
 
-
-
 int main() {
-
     Car car;
     Truck truck;
 
 
-    std:: cout << "Initialized Board game" << std:: endl;
+    std::cout << "Initialized Board game" << std::endl;
     Board board(BOARD_HEIGHT, BOARD_WIDTH);
 
     board.newPrintBoard();
 
-    std:: cout <<std:: endl;
+    std::cout << std::endl;
 
 
-    board.placeCarPiece(car, board.grid, 0, 0);
-    board.placeCarPieceVertical(car, board.grid, 1, 4);
+    board.placeCarPiece(car, board.grid, 0, 0, false);
+    board.placeCarPiece(car, board.grid, 1, 4, true);
 
-    board.placeTruckPiece(truck, board.grid, 2, 2);
-    board.placeTruckPieceVertical(truck, board.grid, 5, 1);
+    board.placeTruckPiece(truck, board.grid, 0, 2, false);
+    board.placeTruckPiece(truck, board.grid, 5, 5, false);
 
 
     board.newPrintBoard();
-
 }
