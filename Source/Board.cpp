321b6760cb83c0a48a0713bb8a11a982c3e26e9e
IvanOG7777 <elderivanargueta04@gmail.com>
2025-08-25 12:14:10 -0700

    Grab Piece function
    Added notes and added some loops to find the anchor point within the vector of pairs in indexPairs. Need to figure out how to see if a piece is vertical or horizontal next.

--- Source/Board.cpp
+++ Source/Board.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include  <map>
 
 Board::Board(int passedHeight, int passedWidth) : height(passedHeight), width(passedWidth) { 
     grid = initializeBoard();
@@ -153,40 +154,69 @@ void Board::grabPiece(std::vector<std::vector<int>> &board, int xCoord, int yCoo
 
     if (yCoord < 0 || yCoord >= row || xCoord < 0 || xCoord >= col) return; //out of bounds check 
 
-    int target = idGrid[yCoord][xCoord];
-    if (target == 0) return;
+    int target = board[yCoord][xCoord]; // target id is the x y coord user selected
+    if (target == 0) return; // if the spot they selected is 0 (not a valid piece) return
     
-    std::vector <std:: vector<bool>> visited(row, std:: vector<bool>(col, false));
-    std:: vector <std::pair<int, int>> indexPair;
-    std::queue<std::pair<int, int>> queue;
+    std::vector <std:: vector<bool>> visited(row, std:: vector<bool>(col, false)); // create a bool map of size height x width so we can check if we've seaerched that index
+    std:: vector <std::pair<int, int>> indexPairs; // create a vector of pairs to hold all pairs that are the same as the target id
+    std::queue<std::pair<int, int>> queue; // create a queue of pair to constanly check new passed pairs until its empty
+    int anchorX = 0;
+    int anchorY = 0;
+    bool isVertical = false;
     
-    queue.push({xCoord, yCoord});
-    visited[yCoord][xCoord] = true;
+    queue.push({xCoord, yCoord}); // push our initial selected x and y coord to the queue
+    visited[yCoord][xCoord] = true; // make visited map at initial x y coord to true meaning its already been searched
 
 
-    while (!queue.empty()) {
-        std::pair<int, int> cxcy = queue.front();
-        queue.pop();
-        indexPair.push_back(cxcy);
+    while (!queue.empty()) { // while the queue is NOT empty
+        std::pair<int, int> currentXYPair = queue.front(); // set currentXYPair to the single pair in queue 
+        queue.pop(); // pop the current pair in the queue to make it ready for next valid pair
+        indexPairs.push_back(currentXYPair); // push the currentXYPair into the vector of valid pairs
 
-        const int dx[4] = { -1, 1, 0, 0 };
-        const int dy[4] = { 0, 0, -1, 1 };
+        const int dx[4] = { -1, 1, 0, 0 }; // created array of indices to search around x
+        const int dy[4] = { 0, 0, -1, 1 }; // created array of indices to search around y
 
         for (int i = 0; i < 4; i++) {
-            int nx = cxcy.first + dx[i];
-            int ny = cxcy.second + dy[i];
+            int nextX = currentXYPair.first + dx[i]; // adding dx at current index to the point we selected
+            int nextY = currentXYPair.second + dy[i]; // adding dy at current index to point we selected
 
-            if (nx < 0 || nx >= col || ny < 0 || ny >= row) continue;
-            if (visited[ny][nx]) continue;
-            if (board[ny][nx] != target) continue;
-            visited[ny][nx] = true;
-            queue.push({ nx, ny });
+            if (nextX < 0 || nextX >= col || nextY < 0 || nextY >= row) continue; // used a as guard. if any of the following is out of bounds in the map we continue, skipping the rest of the code and moving to the next index
+            if (visited[nextY][nextX]) continue; // if we have visited the spot already skip to next index
+            if (board[nextY][nextX] != target) continue; // if the value at index on board does match the target id we skip to the next index
+            visited[nextY][nextX] = true; // if we are able to pass all of the above we've found another id of the same value, we set that index to true on the visited board
+            queue.push({ nextX, nextY }); // then we push that new x and y value into the queue so that we can use those values as our new currentXYPair
+           
         }
     }
 
-    for (auto pair : indexPair) {
-        std::cout << "{" << pair.first << "," << pair.second << "}" << std::endl;
+    std::pair <int, int> firstPair = indexPairs.front();
+    std::cout << firstPair.first << firstPair.second << std::endl;
+
+    int minX = firstPair.first;
+    int maxX = firstPair.first;
+    int minY = firstPair.second;
+    int maxY = firstPair.second;
+
+    for (int i = 1; i < indexPairs.size(); i++) {
+        if (indexPairs[i].first > maxX) {
+            maxX = indexPairs[i].first;
+        }
+
+        if (indexPairs[i].first < minX) {
+            minX = indexPairs[i].first;
+        }
+
+        if (indexPairs[i].second > maxY) {
+            maxY = indexPairs[i].second;
+        }
+
+        if (indexPairs[i].second < minY) {
+            minY = indexPairs[i].second;
+        }
     }
+
+    anchorX = minX;
+    anchorY = minY;
 }
 
 void Board::placeCarPiece(Car &car, std::vector<std::vector<char> > &board, int xCoord, int yCoord, bool isVertical) {
