d70a8c93d111a93dad475396257d12cd69f08760
Ivan <elderivanargueta04@gmail.com>
2025-09-02 12:46:14 -0700

    Update Level.h

--- Headers/Level.h
+++ Headers/Level.h
@@ -1,1 +1,22 @@
-#in
+#ifndef LEVEL_H
+#define LEVEL_H
+
+enum class PieceKind {
+    Car,
+    Truck,
+    RedCar,
+};
+
+struct LevelPiece {
+    PieceKind kind;
+    int id;
+    int x, y;
+    bool isVertical;
+};
+
+struct Level {
+    int width, height;
+    std::vector<LevelPiece> pieces;
+};
+
+#endif //LEVEL_H
